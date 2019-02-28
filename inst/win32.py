d1cee52fad1978883f5c714468a675a4ad7eabb9
Jimmy, Lee <phxswjjj@gmail.com>
2019-03-01 01:13:30 +0800

    keyboard, mouse test

---
+++ inst/win32.py
@@ -0,0 +1,47 @@
+import win32gui
+import win32con
+from PIL import ImageGrab
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+class wnd:
+    def __init__(self):
+        self.hwnd = None
+        self.rect = None
+        self.image = None
+        self.m = PyMouse()
+        self.k = PyKeyboard()
+
+    def load(self, wintitle):
+        self.hwnd = win32gui.FindWindow(None, wintitle)
+        self.rect = win32gui.GetWindowRect(self.hwnd)
+
+    def grab(self):
+        self.focus()
+
+        rect = self.rect
+        self.image = ImageGrab.grab(rect)
+
+    def focus(self):
+        hwnd = self.hwnd
+
+        if(win32gui.GetForegroundWindow() != hwnd):
+            if(not win32gui.IsIconic(hwnd)):
+                win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
+            win32gui.ShowWindow(hwnd, win32con.SW_SHOWNORMAL)
+
+    def abspos(self, x, y):
+        rect = self.rect
+
+        return (rect[0] + x, rect[1] + y)
+
+    def click(self, rx, ry, button = 1, n = 1):
+        rect = self.rect
+        x, y = rect[0] + rx, rect[1] + ry
+        self.m.click(x, y, button, n)
+
+    def click_l(self, rx, ry, n = 1):
+        self.click(rx, ry, n)
+
+    def tap(self, c, n = 1):
+        self.k.tap_key(c, n)
