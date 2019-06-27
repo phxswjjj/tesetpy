6c1895df64e3757214baaba506fd353b46423d9a
Jimmy, Lee <phxswjjj@gmail.com>
2019-06-28 01:03:40 +0800

    add reconnect test case

--- inst/test/test_cadria.py
+++ inst/test/test_cadria.py
@@ -1,7 +1,19 @@
+import cv2
+
 from inst.cadria import wnd
+from inst.scene import FeatureRule
+
+from . import match
+
+disconnect_img = cv2.imread('inst/scene/img/test_reconnect.jpg')
 
 
 def test_wnd():
     w = wnd()
     w.load()
     assert w.hwnd != 0
+
+
+def test_reconnect():
+    rule_reconnect = FeatureRule('reconnect.jpg', 0.9)
+    assert match([rule_reconnect], disconnect_img)
