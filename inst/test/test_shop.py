25db4f5e13402f12204fbffacced74951aa0595e
Jimmy, Lee <phxswjjj@gmail.com>
2019-06-30 14:11:32 +0800

    測試道具完成

--- inst/test/test_shop.py
+++ inst/test/test_shop.py
@@ -5,12 +5,14 @@ from inst.scene import FeatureRule
 from . import match
 
 shop_img = cv2.imread('inst/scene/img/test_shop.jpg')
+shop2_img = cv2.imread('inst/scene/img/test_shop2.jpg')
 pub_img = cv2.imread('inst/scene/img/test_pub.jpg')
 
 def test_collect_res():
     rule_stone = FeatureRule('shop_res_stone.jpg', 0.95)
     rule_magic = FeatureRule('shop_res_magic.jpg')
     assert match([rule_stone, rule_magic], shop_img)
+    assert match([rule_stone, rule_magic], shop2_img)
 
     loc_1st = rule_stone.get_loc_1st(shop_img)
     loc_center = rule_stone.get_loc_center(shop_img)
@@ -19,3 +21,8 @@ def test_collect_res():
     assert loc_center[1] > 100 and loc_center[1] < 175
 
     assert not match([rule_stone, rule_magic], pub_img)
+
+def test_item_completed():
+    rule_item_completed = FeatureRule('item_completed.jpg', 0.95)
+    assert not rule_item_completed.match(shop_img)
+    assert rule_item_completed.match(shop2_img)
